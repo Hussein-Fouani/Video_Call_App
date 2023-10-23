1c6145905cea3394b11c3a18949e2b9ab69146e4
Hussein <social.101.acc@gmail.com>
2023-10-23 15:32:31 +0300

    created some users for demo purpose

--- src/main/java/com/hf/video_call_app/VideoCallAppApplication.java
+++ src/main/java/com/hf/video_call_app/VideoCallAppApplication.java
@@ -1,7 +1,11 @@
 package com.hf.video_call_app;
 
+import com.hf.video_call_app.user.User;
+import com.hf.video_call_app.user.UserService.UserService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VideoCallAppApplication {
@@ -10,4 +14,24 @@ public class VideoCallAppApplication {
         SpringApplication.run(VideoCallAppApplication.class, args);
     }
 
+    @Bean
+    public CommandLineRunner commandLineRunner( UserService service) {
+        return args -> {
+            service.RegisterUser(User.builder().
+                    username("Hsen")
+                            .email("hsen@gmail.com")
+                            .password("AAA")
+                    .build());
+            service.RegisterUser(User.builder().
+                    username("Ali")
+                    .email("Ali@gmail.com")
+                    .password("AAA")
+                    .build());
+            service.RegisterUser(User.builder().
+                    username("Elie")
+                    .email("Elie@gmail.com")
+                    .password("AAA")
+                    .build());
+        };
+    }
 }
