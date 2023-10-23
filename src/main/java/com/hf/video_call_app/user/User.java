f1c920d1927d87b8a3b17c56f883151e427108cc
Hussein <social.101.acc@gmail.com>
2023-10-23 15:34:23 +0300

    user entity

--- src/main/java/com/hf/video_call_app/user/User.java
+++ src/main/java/com/hf/video_call_app/user/User.java
@@ -1,4 +1,13 @@
 package com.hf.video_call_app.user;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class User {
+    private String username;
+    private String password;
+    private String email;
+    private String status;
 }
